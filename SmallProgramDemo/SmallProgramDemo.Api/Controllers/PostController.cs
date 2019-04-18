7536bce82cadd9fc04f54b8c085a3633b2ef8d26
smallprogram <smallprogram@foxmail.com>
2019-04-18 15:09:31 +0800

    Add PaginatedList ，matedata

--- SmallProgramDemo/SmallProgramDemo.Api/Controllers/PostController.cs
+++ SmallProgramDemo/SmallProgramDemo.Api/Controllers/PostController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using SmallProgramDemo.Core.Entities;
 using SmallProgramDemo.Core.Interface;
-using SmallProgramDemo.Infrastructure.Database;
 using SmallProgramDemo.Infrastructure.Resources;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SmallProgramDemo.Api.Controllers
 {
@@ -20,26 +19,52 @@ namespace SmallProgramDemo.Api.Controllers
         private readonly IUnitOfWork unitOfWork;
         private readonly ILogger<PostController> logger;
         private readonly IMapper mapper;
+        private readonly IUrlHelper urlHelper;
 
         public PostController(
             IPostRepository postRepository,
-            IUnitOfWork unitOfWork, 
+            IUnitOfWork unitOfWork,
             ILogger<PostController> logger,
-            IMapper mapper)
+            IMapper mapper,
+            IUrlHelper urlHelper)
         {
             this.postRepository = postRepository;
             this.unitOfWork = unitOfWork;
             this.logger = logger;
             this.mapper = mapper;
+            this.urlHelper = urlHelper;
         }
 
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [HttpGet(Name ="getPosts")]
+        public async Task<IActionResult> Get(PostQueryParameters postQueryParameters)
         {
-            var posts = await postRepository.GetAllPosts();
-            //将posts集合映射为PostResource集合
-            var postResource = mapper.Map<IEnumerable<Post>, IEnumerable<PostResource>>(posts);
+            //返回带有元数据的PaginatedList数据
+            var postsWithMateData = await postRepository.GetAllPosts(postQueryParameters);
+            //将posts集合映射为PostResource集合，取出其中的post数据集合
+            var postResource = mapper.Map<IEnumerable<Post>, IEnumerable<PostResource>>(postsWithMateData);
+
+            var previousPageLink = postsWithMateData.HasPrevious ?
+                CreatePostUri(postQueryParameters, PaginationResourceUriType.PreviousPage,urlHelper, "getPosts") : null;
+
+            var nextPageLink = postsWithMateData.HasNext ?
+                CreatePostUri(postQueryParameters, PaginationResourceUriType.NextPage, urlHelper, "getPosts") : null;
+
+            //提取元数据
+            var mate = new
+            {
+                PageSize = postsWithMateData.PageSize,
+                PageIndex = postsWithMateData.PageIndex,
+                TotalItemCount = postsWithMateData.TotalItemsCount,
+                PageCount = postsWithMateData.PageCount,
+                previousPageLink,
+                nextPageLink
+            };
+
+            //将翻页元数据添加到响应Header的X-Pagination中
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(mate, new JsonSerializerSettings {
+                ContractResolver = new CamelCasePropertyNamesContractResolver() //将所有mate元数据属性首字母小写返回
+            }));
 
             //logger.LogError("测试的错误日志记录");
             //throw new Exception("发生了错误");
@@ -74,6 +99,50 @@ namespace SmallProgramDemo.Api.Controllers
             postRepository.AddPost(post);
             await unitOfWork.SaveAsync();
             return Ok();
+        }
+
+
+
+        /// <summary>
+        /// 生成前一页，后一页，当前页的URI链接方法
+        /// </summary>
+        /// <param name="parameters">页面的元数据</param>
+        /// <param name="uriType">URI类型</param>
+        /// <param name="_urlHelper">URIHelper</param>
+        /// <param name="actionName">需要调用的ActionName</param>
+        /// <returns></returns>
+        private string CreatePostUri(PostQueryParameters parameters, PaginationResourceUriType uriType, IUrlHelper _urlHelper, string actionName)
+        {
+            switch (uriType)
+            {
+                case PaginationResourceUriType.PreviousPage:
+                    var previousParameters = new
+                    {
+                        pageIndex = parameters.PageIndex - 1,
+                        pageSize = parameters.PageSize,
+                        orderBy = parameters.OrderBy,
+                        fields = parameters.Fields
+                    };
+                    return _urlHelper.Link(actionName, previousParameters);
+                case PaginationResourceUriType.NextPage:
+                    var nextParameters = new
+                    {
+                        pageIndex = parameters.PageIndex + 1,
+                        pageSize = parameters.PageSize,
+                        orderBy = parameters.OrderBy,
+                        fields = parameters.Fields
+                    };
+                    return _urlHelper.Link(actionName, nextParameters);
+                default:
+                    var currentParameters = new
+                    {
+                        pageIndex = parameters.PageIndex,
+                        pageSize = parameters.PageSize,
+                        orderBy = parameters.OrderBy,
+                        fields = parameters.Fields
+                    };
+                    return _urlHelper.Link(actionName, currentParameters);
+            }
         }
     }
 }
